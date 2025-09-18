40aed961eb5daae7a7b0315bf38b11ef762ed653
madhurireddygurram1 <23d41a0583@gmail.com>
2025-09-18 11:25:25 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+    }
+}
